2836305ad3964f8805a40a168aa0b80b59433d78
JulioRuiz884 <pr724206@gmail.com>
2024-03-25 21:11:31 -0400

    jwt middleware

--- Coling/Coling.API.Curriculum/Program.cs
+++ Coling/Coling.API.Curriculum/Program.cs
@@ -1,12 +1,13 @@
 using Coling.API.Curriculum.Contratos.Repositorios;
 using Coling.API.Curriculum.Implementacion.Repositorios;
+using Coling.Utilitarios.Middlewares;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
@@ -17,6 +18,11 @@ var host = new HostBuilder()
         services.AddScoped<IInstitucionRepositorio, InstitucionRepositorio>();
         services.AddScoped<IProfesionRepositorio, ProfesionRepositorio>();
         services.AddScoped<ITipoEstudioRepositorio, TipoEstudioRepositorio>();
+        services.AddSingleton<JwtMiddleware>();
+    })
+    .ConfigureFunctionsWebApplication(x =>
+    {
+        x.UseMiddleware<JwtMiddleware>();
     })
     .Build();
 
